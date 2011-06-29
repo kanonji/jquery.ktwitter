c2e043a6e520853b5e34a026ffcc0f4b8b49281b
kanonji <kanonji@localhost>
2011-06-30 07:13:41 +0900

    rendering

--- asset/js/jquery.ktwitter.js
+++ asset/js/jquery.ktwitter.js
@@ -13,10 +13,12 @@
         }
       });
       console.info(this);
+      console.info(arguments);
       return methods.load.apply( this, arguments );
     },
     load: function( options ) {
       console.info(this);
+      var $this = this;
       return this.each(function(){
         if ( options ) {
           extend( settings, options );
@@ -27,6 +29,7 @@
           count: settings.count
         }, function( result ){
           element.ktwitters = result;
+          methods.render.apply( $this, arguments );
         });
       });
     },
@@ -34,12 +37,67 @@
       return this.each(function(){
         //TODO stab
       });
+    },
+    render: function(){
+      console.info(this);
+      console.info(arguments);
+      var data = this.get(0).ktwitters;
+      var ul = $('<ul>');
+      var template = '<li class="clearfix"><img src="{{icon}}" class="picture"><div class="text"><p class="tweet"><span>{{screen_name}}</span>{{text}}</p><p class="date">{{datetime}}</p></div></li>';
+      var item;
+      for( var i in data ){
+        item = {
+          icon: data[i].user.profile_image_url,
+          screen_name: data[i].user.screen_name,
+          text: data[i].text,
+          datetime: _relative_time(data[i].created_at)
+        };
+        ul.append(_render(item, template));
+      }
+      this.append(ul);
+    },
+    setRender: function( func ){
+      methods.render = func;
     }
   };
 
   var extend = function( settings, options ) {
     return $.extend( settings, options );
   };
+
+  function _relative_time(time_value) {
+    var values = time_value.split(" ");
+    time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+    var parsed_date = Date.parse(time_value);
+    var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+    var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+    delta = delta + (relative_to.getTimezoneOffset() * 60);
+
+    if (delta < 60) {
+      return 'less than a minute ago';
+    } else if(delta < 120) {
+      return 'about a minute ago';
+    } else if(delta < (60*60)) {
+      return (parseInt(delta / 60)).toString() + ' minutes ago';
+    } else if(delta < (120*60)) {
+      return 'about an hour ago';
+    } else if(delta < (24*60*60)) {
+      return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+    } else if(delta < (48*60*60)) {
+      return '1 day ago';
+    } else {
+      return (parseInt(delta / 86400)).toString() + ' days ago';
+    }
+  }
+
+  function _render(item, template){
+    for(var key in item){
+      item[key] = (item[key] === undefined) ? '' : item[key];
+      var rgx = new RegExp('{{' + key + '}}', 'g');
+      template = template.replace(rgx, item[key]);
+    }
+    return template;
+  }
 
   $.fn.ktwitter = function( method ){
     if ( methods[method] ) {
